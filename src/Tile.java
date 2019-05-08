5cd4803c02df43f67fee41a8445385674b0336ce
SebastianLindgren <sebastian.lindgren1994@gmail.com>
2019-05-08 14:44:13 +0200

    Updated tile drawing (#15)

--- src/Tile.java
+++ src/Tile.java
@@ -13,16 +13,19 @@ public class Tile implements CustGraphics {
         this.row = row;
     }
 
-    public void draw(int x, int y, int width, int height, Graphics graphics) {
+    public void draw(int x, int y, int rec_size, Graphics g) {
         // TODO: x and y should be calculated by Tile based on `column` and
         // `row `, instead of passed as arguments
-        graphics.drawRect(x, y, width, height);
+        g.drawRect(x+5, y+5, rec_size-10, rec_size-10);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.fillRect(x+7, y+7, rec_size-10, rec_size-10);
 
         String text = Integer.toString(this.value);
-        FontMetrics metrics = graphics.getFontMetrics();
-        int valueX = x + (width - metrics.stringWidth(text)) / 2;
-        int valueY = y + (height - metrics.getHeight()) / 2 + metrics.getAscent();
-        graphics.drawString(text, valueX, valueY);
+        FontMetrics metrics = g.getFontMetrics();
+        int valueX = x + (rec_size - 10 - metrics.stringWidth(text)) / 2;
+        int valueY = y + (rec_size - 10 - metrics.getHeight()) / 2 + metrics.getAscent();
+        g.drawString(text, valueX, valueY);
     }
 
     public void increment() {
