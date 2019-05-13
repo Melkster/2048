d44a80a030288e01e71490af7919e7a77dd698f8
Sebastian <sebastian.lindgren1994@gmail.com>
2019-05-13 16:54:10 +0200

    Updated Color for Tile

---
+++ src/TileColor.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.awt.Color;
+
+public class TileColor {
+    public static Color tileColor(int value){
+
+        if (value == 2) {return new Color(0xeee4da);}
+        if (value == 4) {return new Color(0xede0c8);}
+        if (value == 8) {return new Color(0xf2b179);}
+        if (value == 16) {return new Color(0xf59563);}
+        if (value == 32) {return new Color(0xf67c5f);}
+        if (value == 64) {return new Color(0xf65e3b);}
+        if (value == 128) {return new Color(0xedcf72);}
+        if (value == 256) {return new Color(0xedcc61);}
+        if (value == 512) {return new Color(0xedc850);}
+        if (value == 1024) {return new Color(0xedc53f);}
+        if (value == 2048) {return new Color(0xedc22e);}
+        
+        return Color.GRAY;
+    }
+}
