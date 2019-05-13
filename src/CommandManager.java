8ca0756796e6389f2567c6819d71cdf96288fe0d
Melker Österberg <melkerosterberg@gmail.com>
2019-05-14 01:12:09 +0200

    Implement SpawnTile and State.iterator()

--- src/CommandManager.java
+++ src/CommandManager.java
@@ -10,42 +10,54 @@ public class CommandManager {
     private Stack<Command> redoStack = new Stack<Command>();
 
 
- 	//Executes a Command instance and adds it to the list of undos that are available.
+    /**
+     * Executes a Command instance and adds it to the list of undos that are available.
+     */
     public void executeCommand(Command command) {
         command.execute();
         undoStack.push(command);
         redoStack.clear();
     }
 
-    //Checks if there is at least one undoable Command available on the undo list.
+    /**
+     * Checks if there is at least one undoable Command available on the undo list.
+     */
     public boolean isUndoAvailable() {
         return !undoStack.empty();
     }
 
-    /** Undoes the next available command to undo.
-     * If four commands were executed, the undo operations for those commands will
-     * happen in reverse order with four calls to this method.
+    /**
+     * Undoes the next available command to undo, if there is one.
      */
      public void undoCommand() {
-     	assert(!undoStack.empty());
-        Command command = undoStack.pop();
-        command.undo();
-        redoStack.push(command);
+         if (!undoStack.empty()) {
+            Command command = undoStack.pop();
+            command.undo();
+            redoStack.push(command);
+        }
     }
 
-    //Checks if there is at least one undoable Command available on the undo list.
+    /**
+     * Checks if there is at least one undoable Command available on the undo list.
+     */
     public boolean isRedoAvailable() {
-		return !redoStack.empty();
-	}
-
-	//Redoes the next available command to redo.
-   	public void redoCommand() {
-        assert(!redoStack.empty());
-        Command command = redoStack.pop();
-        command.execute();
-        undoStack.push(command);
+        return !redoStack.empty();
     }
 
+    /**
+     * Redoes the next available command to redo, if there is one.
+     */
+    public void redoCommand() {
+        if (!redoStack.empty()) {
+            Command command = redoStack.pop();
+            command.execute();
+            undoStack.push(command);
+        }
+    }
+
+    /**
+     * Removes all available redo Commands.
+     */
     public void clearRedos() {
         redoStack.clear();
     }
--- src/SpawnTile.java
+++ src/SpawnTile.java
@@ -1,32 +1,48 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
- * Spawns a Tile at a random position in `state`, with either the value 2 or 4
- * (chosen randomly).
+ * Spawns a Tile at a random empty (Void) position in `state`, with either the value 2 or 4
+ * (chosen randomly). Pre condition: Assumes that there is an empty position in `state`.
  */
 public class SpawnTile implements Command {
     private int column;
     private int row;
-    private State previousState;
+    private int value;
+    private State state;
 
     public SpawnTile(State state) {
-        this.column = column;
-        this.row = row;
-        this.previousState = state;
+        this.state = state;
+
+        ArrayList<Tile> voids = state.getEmptyTiles();
+        Random rand = new Random();
+        Tile randomEmptyTile = voids.get(rand.nextInt(voids.size())); // Selects one of the `state`s Voids at random
+
+        this.column = randomEmptyTile.column;
+        this.row = randomEmptyTile.row;
+        this.value = randomValue();
     }
 
     @Override
     public void execute() {
-        // TODO
+        state.addTile(column, row, value);
     }
 
     @Override
     public void undo() {
-        // TODO
+        state.removeTile(column, row);
     }
 
     @Override
     public void redo() {
-        // TODO
+        execute();
+    }
+
+    private int randomValue() {
+        int randomValue = new Random().nextInt(100);
+        if (randomValue < 75) return 2;
+        else return 4;
     }
 }
