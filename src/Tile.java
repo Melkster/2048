866dbb4f9a562f54c75b251548df5b7706b7da8e
Melkster <melkerosterberg@gmail.com>
2019-05-13 12:50:31 +0200

    Fix a number of minor bugs and compilation errors (#20)
    * WIP Implement neighbourTile() and push()
    
    * Initialize State with an empty board
    
    Fix some bugs in State
    
    * Minor fixup of Swipe
    
    Change order of arguments to Tile
    
    * Implement Swipe.execute() (#19)
    
    * Fix a number of minor bugs and compilation errors

--- src/Tile.java
+++ src/Tile.java
@@ -2,7 +2,7 @@ package src;
 
 import java.awt.*;
 
-public class Tile implements CustGraphics {
+public class Tile implements CustGraphics, Cloneable {
     public int value;
     public int column;
     public int row;
@@ -13,19 +13,19 @@ public class Tile implements CustGraphics {
         this.row = row;
     }
 
-    public void draw(int x, int y, int rec_size, Graphics g) {
+    public void draw(int x, int y, int height, int width, Graphics graphics) {
         // TODO: x and y should be calculated by Tile based on `column` and
         // `row `, instead of passed as arguments
-        g.drawRect(x+5, y+5, rec_size-10, rec_size-10);
-        Graphics2D g2 = (Graphics2D) g;
+        graphics.drawRect(x + 5, y + 5, width - 10, height - 10);
+        Graphics2D g2 = (Graphics2D) graphics;
         g2.setColor(Color.BLACK);
-        g2.fillRect(x+7, y+7, rec_size-10, rec_size-10);
+        g2.fillRect(x + 7, y + 7, width - 10, height - 10);
 
         String text = Integer.toString(this.value);
-        FontMetrics metrics = g.getFontMetrics();
-        int valueX = x + (rec_size - 10 - metrics.stringWidth(text)) / 2;
-        int valueY = y + (rec_size - 10 - metrics.getHeight()) / 2 + metrics.getAscent();
-        g.drawString(text, valueX, valueY);
+        FontMetrics metrics = graphics.getFontMetrics();
+        int valueX = x + (width - 10 - metrics.stringWidth(text)) / 2;
+        int valueY = y + (height  - 10 - metrics.getHeight()) / 2 + metrics.getAscent();
+        graphics.drawString(text, valueX, valueY);
     }
 
     public void increment() {
@@ -37,7 +37,18 @@ public class Tile implements CustGraphics {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (o instanceof Tile) return this.value == ((Tile) o).value;
+        else return false;
+    }
+
+    @Override
     public String toString() {
         return "<" + value + ">";
+    }
+
+    @Override
+    public Tile clone() throws CloneNotSupportedException {
+        return (Tile) super.clone();
     }
 }
